0c8f5be8c30f79c509683211d5f6ca4e68e321ff
Felipe <felipe.unip.adm2015@gmail.com>
2021-03-08 17:37:22 -0300

    Organizando aula 4

---
+++ meuapp/src/aula4/aula4.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from './Form/Form'
+const aula4 = () => {
+    return (
+        <div>
+            <Form />
+        </div>
+    )
+}
+
+export default aula4
